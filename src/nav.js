c36eae4b412a054430ad8e9e5d924ee8fa46f793
Hardik Nahata <hardiknahata@gmail.com>
2022-11-05 19:47:37 -0400

    Lab 7 Completed

--- src/nav.js
+++ src/nav.js
@@ -4,10 +4,29 @@ function Nav() {
  return (
    <div>
      <Link to="/">Labs</Link> |
-     <Link to="/hello">Hello</Link> |
+     <Link to="/">Assignment 6</Link> |
+     <Link to="/a7">Assignment 7</Link> |
      <Link to="/tuiter/home">Tuiter</Link>
    </div>
- )
+ );
 }
 
 export default Nav;
+
+
+
+
+
+// import {Link} from "react-router-dom";
+
+// function Nav() {
+//  return (
+//    <div>
+//      <Link to="/">Labs</Link> |
+//      <Link to="/hello">Hello</Link> |
+//      <Link to="/tuiter/home">Tuiter</Link>
+//    </div>
+//  )
+// }
+
+// export default Nav;
